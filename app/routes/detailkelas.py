0b01e52f610d2745b48e5bafa1e810aec3178448
dikhmasir22 <almasird@gmail.com>
2024-06-06 20:14:32 +0700

    fungsionalitas Detail Kelas

--- app/routes/detailkelas.py
+++ app/routes/detailkelas.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, current_app
 import jwt
+from bson import ObjectId
 
 detailkelas_ = Blueprint('detailkelas', __name__)
 
-@detailkelas_.route('/detailkelas')
-def detailkelas():
+@detailkelas_.route('/detailkelas/<_id>')
+def detailkelas(_id):
     TOKEN_KEY = current_app.config['TOKEN_KEY']
     SECRET_KEY = current_app.config['SECRET_KEY']
     token_receive = request.cookies.get(TOKEN_KEY)
@@ -14,9 +15,10 @@ def detailkelas():
             SECRET_KEY,
             algorithms=['HS256']
         )
+        kelas = current_app.db.semuakelas.find_one({'_id' : ObjectId(_id)})
         user_info = current_app.db.user.find_one({'email': payload.get('id')})
         status = payload.get('id')
-        return render_template('template/detailkelas.html', status = status, user_info=user_info)
+        return render_template('template/detailkelas.html', status = status, user_info=user_info, kelas = kelas)
     except jwt.ExpiredSignatureError:
         msg = request.args.get('msg')
         return render_template('template/detailkelas.html', msg=msg)
