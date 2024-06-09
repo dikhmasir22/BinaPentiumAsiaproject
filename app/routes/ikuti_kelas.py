ef993f20fc0642e98a9dab944aed0a25ac6d6e22
dikhmasir22 <almasird@gmail.com>
2024-06-09 11:06:55 +0700

    update lagi

--- app/routes/ikuti_kelas.py
+++ app/routes/ikuti_kelas.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, current_app, request, redirect, url_for
 import jwt
+from bson import ObjectId
 
 ikuti_kelas_ = Blueprint('ikuti_kelas', __name__)
 
 @ikuti_kelas_.route('/ikuti_kelas/<_id>/<_idsiswa>',methods=['POST'])
-def siswa(_id,_idsiswa):
+def ikuti_kelas(_id,_idsiswa):
     TOKEN_KEY = current_app.config['TOKEN_KEY']
     SECRET_KEY = current_app.config['SECRET_KEY']
     token_receive = request.cookies.get(TOKEN_KEY)
@@ -14,7 +15,8 @@ def siswa(_id,_idsiswa):
             SECRET_KEY,
             algorithms=['HS256']
         )
-        kelas = current_app.db.semuakelas.find_one
+        kelas = current_app.db.semuakelas.find_one({'_id' : ObjectId(_id)})
+        
     except jwt.ExpiredSignatureError:
         msg = 'Your Token Has Expired'
         return redirect(url_for('homepage.homepage', msg=msg))
