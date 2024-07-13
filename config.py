1032e737e02a549f2548b8aac80681339ad4f6bd
dikhmasir22 <56808112+dikhmasir22@users.noreply.github.com>
2024-07-13 23:47:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     SECRET_KEY = 'PentiumGoldProyekAkhir'
     TOKEN_KEY = 'BinaAsiaDigitalindo'
-    MONGODB_URI = 'mongodb+srv://binadigitalindo:1234@binadigitalindocluster.ew6v3ud.mongodb.net/?retryWrites=true&w=majority&appName=binadigitalindocluster'
-    # MONGODB_URI = 'mongodb+srv://dava:123@cluster0.67ahwvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    MONGODB_URI = 'mongodb+srv://binadigitalind:1234@binadigitalindocluster.ew6v3ud.mongodb.net/?retryWrites=true&w=majority&appName=binadigitalindcluster'
+  
     DBNAME = 'BinaAsiaDigitalindo'
     # google
     GOOGLE_CLIENT_ID = '57554680057-dq9jles7nseck6k15m6ca8nra1nmgi9c.apps.googleusercontent.com'
